b8ab55eb6e0721f87cf546121fdd79593496dbb5
cris <criston2011@gmail.com>
2019-12-12 13:46:12 +0530

    :)

---
+++ lib/clock_body.dart
@@ -0,0 +1,31 @@
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:clock_lenovo/clock_face.dart';
+
+class ClockBody extends StatelessWidget{
+
+  ClockBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return new AspectRatio(
+        aspectRatio: 1.0,
+        child: new Stack(
+            children: <Widget>[
+
+              new Container(
+                width: double.infinity,
+                decoration: new BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.black,
+
+                ),
+                child: new ClockFace(),
+              )
+            ]
+        )
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:clock_lenovo/clock.dart';
+import 'package:clock_lenovo/clock_text.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Clock',
+      theme: new ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        primarySwatch: Colors.blue,
+      ),
+
+      home: new AppClock(),
+    );
+  }
+}
+
+
+class AppClock extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Padding(
+        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            new Clock(
+              circleColor: Colors.lightGreen,
+              showBellsAndLegs: false,
+              bellColor: Colors.green,
+              clockText: ClockText.arabic,
+              showHourHandleHeartShape: false,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
